572a908b4fea7d64dbeb6b9fbbce8d81061eafd2
pedromartinsdev <pedrohenriquemartins@live.com>
2021-11-03 23:36:46 -0300

    add and configure cors and add public/index.html

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import "dotenv/config"
 import express from "express";
+import http from "http"
+import cors from "cors"
+
+import { Server, Socket } from "socket.io";
+
 import { router } from "./routes";
 
 const app = express()
+app.use(cors())
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
 app.use(express.json())
 
 app.use(router)
@@ -17,4 +36,4 @@ app.get("/signin/callback", (request, response) => {
     return response.json(code)
 })
 
-app.listen(4000, () => console.log("🚀 Server is running on PORT 4000!"))
+serverHttp.listen(4000, () => console.log("🚀 Server is running on PORT 4000!"))
