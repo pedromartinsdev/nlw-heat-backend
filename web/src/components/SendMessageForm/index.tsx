df1be25211f2790f72c2b187d571df35076f14ae
pedromartinsdev <pedrohenriquemartins@live.com>
2021-12-16 01:38:45 -0300

    sendMessageForm style

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,10 +1,25 @@
-import { useContext } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
 import { AuthContext } from '../../contexts/auth'
+import { api } from '../../services/api'
 import styles from './styles.module.scss'
 
 export function SendMessageForm() {
     const { user, signOut } = useContext(AuthContext)
+    const [message, setMessage] = useState('')
+
+
+    async function handleSendMessage(event: FormEvent) {
+        event.preventDefault();
+
+        if(!message.trim()){
+            return;
+        }
+        
+        await api.post('messages', {message})
+
+        setMessage('')
+    }
 
     return (
         <div className={styles.sendMessageFormWrapper}>
@@ -23,12 +38,14 @@ export function SendMessageForm() {
                 </span>
             </header>
 
-            <form className={styles.sendMessageForm}>
+            <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
                 <label htmlFor="message">Messagem</label>
                 <textarea 
                     name="message" 
                     id="message" 
                     placeholder="Qual sua expectativa para o evento?"
+                    onChange={event => setMessage(event.target.value)}
+                    value={message}
                  />
 
                  <button type='submit'> Enviar mensagem </button>
